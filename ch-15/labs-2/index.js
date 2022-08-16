d3159a6e95fb507e6c90fcb30184fdbd1a5aa834
azamb <azambrano@hashtagpaid.com>
2022-08-15 23:23:30 -0400

    Chapter 15: Child Processes

--- ch-15/labs-2/index.js
+++ ch-15/labs-2/index.js
@@ -3,7 +3,8 @@
 const { spawn } = require('child_process')
 
 function exercise (command, args) {
-  return spawn(command, args)
+  // https://nodejs.org/docs/latest-v16.x/api/child_process.html#optionsstdio
+  return spawn(command, args, { stdio: ['ignore', 'inherit', 'pipe']})
 }
 
 module.exports = exercise
