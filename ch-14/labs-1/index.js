d3749b1efc71554e17997470e262cb7dd5d2a05e
azamb <azambrano@hashtagpaid.com>
2022-08-17 20:56:06 -0400

    Chapter 14: Process and OS

---
+++ ch-14/labs-1/index.js
@@ -0,0 +1,8 @@
+/*
+* In index.js use console.log to output the operatingsystem identifier
+*
+* Ensure the processexits with a non-zero exit code.
+*/
+console.log(process.platform) // https://nodejs.org/docs/latest-v16.x/api/process.html#processplatform
+
+process.exit(3) // https://nodejs.org/docs/latest-v16.x/api/process.html#exit-codes
